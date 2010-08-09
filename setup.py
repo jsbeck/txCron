29d579ae9f0be0df3393f9c2326fa31c88a22c9b
Jason Beck <jason.s.beck@gmail.com>
2010-08-09 15:03:28 +0000

    apply fixes to the setup script

--- setup.py
+++ setup.py
@@ -5,13 +5,9 @@ version = '1.0rc1'
 setup(name='txcron',
       version=version,
       description="Schedule at-style, cron-style or interval jobs in Twisted",
-      author='Jason Beck'
-      author_email='jason.s.beck@gmail.com',
+      author='Jason Beck',
+      author_email='jsbeck@subfx.net',
       url='http://subfx.net',
       license='MIT',
       packages=['txcron',],
-      install_requires=[
-          'Twisted>=8',
-          'zope.interface',
-      ])
-
+      package_dir={'txcron':'src/txcron',})
